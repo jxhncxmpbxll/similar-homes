89a2957e8c8c3915e5ab7f972c16593f9198576b
John <j0hn4ll3nc4mpb3ll@gmail.com>
2020-11-13 22:59:45 -0500

    adjusts code formatting

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/api/similar', (req, res) => {
-  db.fetchHomes().then(result => res.status(200).send(result))
+  db.fetchHomes()
+  .then(result => res.status(200).send(result))
   .catch(err => {
     console.log(err);
     res.status(400).send(err);
